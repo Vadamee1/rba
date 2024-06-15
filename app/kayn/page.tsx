a91fe51e13f61c94a434a9c8bc4a3700348fcafd
David <david.hernandez.v44@gmail.com>
2024-06-15 16:52:37 -0600

    feat(metadata): add icons

--- app/kayn/page.tsx
+++ app/kayn/page.tsx
@@ -1,6 +1,15 @@
 import CharacterCard from "@/components/shared/CharacterCard";
 import { kayn } from "@/constants/kayn";
 import { Character } from "@/types/characters";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Vadam - RBA",
+  description: "Presentación para la comunidad de roleplay",
+  icons: {
+    icon: "/IconKayn.png",
+  },
+};
 
 export default function Kayn() {
   const { body, eyes, characterData, footer, link }: Character = kayn;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,18 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
 import { Providers } from "./providers";
 
-import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
 import AboutMe from "@/components/aboutMe";
 import Accounts from "@/components/accounts";
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
+  title: "Vadam - RBA",
+  description: "Presentación para la comunidad de roleplay",
   icons: {
-    icon: "/favicon.ico",
+    icon: "/Icon.png",
   },
 };
 
