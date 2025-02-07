135218264c6d8ceddeeda8d20ad20cd76eb6e171
David Hernández Martínez <david.hernandez@teit.cfe.mx>
2025-02-07 15:48:55 -0600

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { HeroHighlight } from "@/components/ui/hero-highlight";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
-        {children}
+        <HeroHighlight>
+          {children}
+        </HeroHighlight>
       </body>
     </html>
   );
